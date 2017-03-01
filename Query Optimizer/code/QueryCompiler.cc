3074c56eb1b1eaf4ce1fc2e2134201bfd92e1122
Yun <ychang29@ucmerced.edu>
2017-03-01 01:48:33 -0800

    GroupBy, Sum, Project, Project->DuplicateRemoval
    Should print stuff except 1 table case. Will work on that tomorrow. If
    it doesn't print, then the print is wrong or the RelOp constructor is
    wrong (trust me)

--- Query Optimizer/code/QueryCompiler.cc
+++ Query Optimizer/code/QueryCompiler.cc
@@ -234,78 +234,69 @@ void QueryCompiler::Compile(TableList* _tables, NameList* _attsToSelect,
 
 	// connect everything in the query execution tree and return
 	// Create WriteOut here
-	/*
 	WriteOut* writeout;
-	*/
+
 	// ASSUME: Final Join's Schema = s1; Final Join's Pointer = j
 	if (_groupingAtts != 0)														// Non-empty _groupingAtts -> GroupBy
 	{
 		// Create GroupBy here
-		/*
+
 		Schema _schemaIn = s1;												// Set it equal to join's final schema
 		Schema _schemaOut;														// Set it equal to nothing (not really important at the moment)
 		vector<Attribute> att = _schemaIn.GetAtts();							// Get the Attributes
 		int _atts_no = att.size();												// Get the Attribute size
 		int _atts[_atts_no];													// Create an array of indices of Attributes
-		for(int i = 0; i < att.size(); i++)
+		for (int i = 0; i < att.size(); i++)
 			_atts[i] = _schemaIn.Index(att[i].name);							// Receive indices of all Attributes in the Schema
 		OrderMaker _groupingAtts = OrderMaker(_schemaIn, _atts, _atts_no);		// Insert all relevant values into OrderMaker
 		Function _compute;
-		_compute.GrowFromParseTree (_finalFunction, _schemaIn);					// Insert all relevant values into Function
-		GroupBy* groupby = new GroupBy(_schemaIn, _schemaOut, _groupingAtts, _compute,	j);		// j = Final join operator
+		_compute.GrowFromParseTree(_finalFunction, _schemaIn);					// Insert all relevant values into Function
+		GroupBy* groupby = new GroupBy(_schemaIn, _schemaOut, _groupingAtts, _compute, j);		// j = Final join operator
 		string outFile = "outfile";
 		writeout = new WriteOut(_schemaOut, outFile, groupby);						// Insert all relevant values into WriteOut
 																					// outFile is "outfile" because we are not using it yet
-		*/
+
 	}
 	else
 	if (_finalFunction != 0)													// Non-empty _finalFunction -> Sum
 	{
 		// Create Sum here
-		/*
 		Schema _schemaIn = s1;													// Set it equal to join's final schema
 		Schema _schemaOut;														// Set it equal to nothing (not really important at the moment)
 		Function _compute;
-		_compute.GrowFromParseTree (_finalFunction, _schemaIn);				// Insert all relevant values into Function
-		Sum* sum = new Sum(_schemaIn, _schemaOut, _compute,	j);				// j = Final join operator
+		_compute.GrowFromParseTree(_finalFunction, _schemaIn);				// Insert all relevant values into Function
+		Sum* sum = new Sum(_schemaIn, _schemaOut, _compute, j);				// j = Final join operator
 		string outFile = "outfile";
 		writeout = new WriteOut(_schemaOut, outFile, sum);							// Insert all relevant values into WriteOut
 																					// outFile is "outfile" because we are not using it yet
-		*/
 	}
 	else																		// Project or Project + DuplicateRemoval
 	{
 		// Create Project here
-		/*
 		Schema _schemaIn = s1;													// Set it equal to join's final schema
 		Schema _schemaOut;														// Set it equal to nothing (not really important at the moment)
 		int _numAttsInput = _schemaIn.GetAtts().size();							// Get input size
 		int _numAttsOutput = 0;													// Set it equal to 0 because we aren't doing that yet
 		int _keepMe[_numAttsOutput];											// Same as above
 		Project* project = new Project(_schemaIn, _schemaOut, _numAttsInput, _numAttsOutput, _keepMe, j);		// ... = Final join operator
-		*/
 		if (_distinctAtts != 0)													// _distinctAtts != 0 -> DuplicateRemoval
 		{
 			// Create DuplicateRemoval
-			/*
 			DuplicateRemoval* duplicateRemoval = new DuplicateRemoval(_schemaIn, project);
 			string outFile = "outfile";
 			writeout = new WriteOut(_schemaOut, outFile, duplicateRemoval);			// Insert all relevant values into WriteOut
-			// outFile is "outfile" because we are not using it yet
-			*/
+																					// outFile is "outfile" because we are not using it yet
 		}
 		else
 		{
-			/*
 			string outFile = "outfile";
 			writeout = new WriteOut(_schemaOut, outFile, project);					// Insert all relevant values into WriteOut
 																					// outFile is "outfile" because we are not using it yet
-			*/
 		}
-		// Create the QueryExecutionTree
-		/*
-		_queryTree = writeout;
-		*/
 	}
+	// Create the QueryExecutionTree
+	//cout <<"bah" <<writeout <<endl;
+	_queryTree = QueryExecutionTree();
+	_queryTree.SetRoot(*writeout);
 	// free the memory occupied by the parse tree since it is not necessary anymore
 }
